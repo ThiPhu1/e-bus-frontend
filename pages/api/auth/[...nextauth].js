baee8a4490b4a92dbdd69026b7278186ec172848
Thi Ngọc Phú <thingphu1012@gmail.com>
2023-02-01 23:13:04 +0700

    fix: nextauth secret env

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -88,7 +88,7 @@ export const authOptions = {
             return session
         }
     },
-    secret: process.env.NEXT_PUBLIC_NEXT_AUTH_SECRET
+    secret: process.env.NEXTAUTH_SECRET,
 }
 
 export default NextAuth(authOptions);
