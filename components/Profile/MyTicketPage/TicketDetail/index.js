a5796ffe7a34f6cbf6b45170cfeca5059f72ae2f
Thi Ngọc Phú <thingphu1012@gmail.com>
2023-02-02 23:14:36 +0700

    style: ticket detail

--- components/Profile/MyTicketPage/TicketDetail/index.js
+++ components/Profile/MyTicketPage/TicketDetail/index.js
@@ -1,7 +1,26 @@
 import styles from "./styles.module.scss";
 import Image from "next/image";
+import { useMemo } from "react";
+import moment from "moment";
 
 export default function TicketDetail({ data }) {
+    const ticketInfo = useMemo(() => {
+        return [
+            {
+                title: "Tuyến",
+                value: data?.route?.route_number,
+            },
+            {
+                title: "Tên tuyến",
+                value: data?.route?.route_name,
+            },
+            {
+                title: "Ngày mua",
+                value: moment(data?.createdAt).format('h:mm - DD/MM/YYYY'),
+            },
+        ]
+    }, [data])
+
     return (
         <div className={styles["ticket-detail"]}>
             <div className={styles["ticket-detail__code"]}>
@@ -12,6 +31,20 @@ export default function TicketDetail({ data }) {
                     height={180}
                 />
             </div>
+            <ul className={styles["ticket-detail__info"]}>
+                {
+                    ticketInfo?.map((item, index) => {
+                        return <li
+                            className={styles["ticket-detail__info-item"]}
+                            key={index}
+                        >
+                            <span className={styles["ticket-detail__info-item-title"]}>{item?.title}</span>
+                            <span className={styles["ticket-detail__info-item-value"]}>{item?.value}</span>
+                        </li>
+                    })
+                }
+            </ul>
+            <hr />
             <span className={styles["ticket-detail__notify"]}>Vui lòng xuất trình mã QR khi tham gia phương tiện</span>
         </div>
     );
